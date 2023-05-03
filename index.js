d470e30a015e18f9f68a2bf47f89c0ad92ed30fb
GabrielGit0222 <7013218364gabriel@gmail.com>
2023-05-03 07:15:19 +0000

    solution

---
+++ index.js
@@ -0,0 +1,21 @@
+const addDays = require("date-fns/addDays");
+
+function getDate(days) {
+  let newDate = addDays(new Date(2020, 7, 22), days);
+  return `${newDate.getDate()}-${
+    newDate.getMonth() + 1
+  }-${newDate.getFullYear()}`;
+}
+
+module.exports = getDate;
+
+/* 
+const getDate = (days) => {
+  const newDate = addDays(new Date(2020, 7, 22), days);
+  return `${newDate.getDate()}-${
+    newDate.getMonth() + 1
+  }-${newDate.getFullYear()}`;
+};
+
+module.exports = getDate;
+*/
